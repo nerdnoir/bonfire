5f290fe22f8ae12281952e07651be369e4b44cd0
David Laribee <laribee@gmail.com>
2017-10-19 08:31:40 -0400

    Started

--- app/model/event.js
+++ app/model/event.js
@@ -1,7 +1,11 @@
-class Event {
-  
-  ticketCount () {
+function createEvent(candidateEvent, save = (domainEvent) => {}) {
+  if (!candidateEvent.title) throw "Title is required."
+  if (candidateEvent.title.length > 60) throw "Title is required."
+  const eventCreated = {
+    title: candidateEvent.title
   }
+  save(eventCreated)
 }
 
-module.exports = Event
+
+module.exports = { createEvent }
